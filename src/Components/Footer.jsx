60c8b6d205937538a65233ea72f91c1ef1f7a92f
Alexis.Avril <alexisavril@sfr.fr>
2025-09-10 07:06:00 +0200

    Changes on Header and Footer, work on logos

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -1,7 +1,10 @@
+import logo from '../assets/pictures/logo.svg';
+import '../styles/Footer.scss';
+
 function Footer() {
   return (
-    <footer style={{ textAlign: "center", padding: "1rem", backgroundColor: "#1a1a1a", color: "#fff" }}>
-        <h2>Kasa</h2>
+    <footer className="footer">
+        <div className="footer-logo"><img src={logo} alt="Kasa Logo" /></div>
       <p>&copy; 2020 Kasa. All rights reserved.</p>
     </footer>
   );
