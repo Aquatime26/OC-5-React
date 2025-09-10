f1bde1e615604f9d6661ad4b5047e08927104d76
Alexis.Avril <alexisavril@sfr.fr>
2025-09-10 06:44:27 +0200

    Animations and fixing on Dropdowns

--- src/Components/Dropdown.jsx
+++ src/Components/Dropdown.jsx
@@ -1,12 +1,31 @@
+import { useState, useRef } from 'react';
 import '../styles/Dropdown.scss';
 
 function Dropdown({ title, children }) {
+  const [isOpen, setIsOpen] = useState(false);
+  const contentRef = useRef(null);
+
+  const toggleDropdown = (e) => {
+    e.stopPropagation();
+    setIsOpen(!isOpen);
+  }
   
   return (
-    <details className="dropdown">
-      <summary className="dropdown-title">{title}</summary>
-      <div className="dropdown-content">{children}</div>
-    </details>
+    <div className="dropdown">
+      <div className="dropdown-title">{title}
+        <button className="dropdown-button" aria-label="Toggle Dropdown" onClick={toggleDropdown}>
+          <i className={`fa-solid fa-chevron-up ${isOpen ? 'open' : ''}`}></i>
+        </button>
+      </div>
+      <div 
+        ref={contentRef}
+        className={`dropdown-content ${isOpen ? 'open' : ''}`}
+        style={{
+          maxHeight: isOpen ? `${contentRef.current.scrollHeight}px` : '0px'
+        }}>
+        {children}
+      </div>
+    </div>
   );
 }
 
